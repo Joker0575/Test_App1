2cfb63feeaecc6129334e2b288a562d145925b34
joker0575 <raju.msit05@gmail.com>
2015-09-25 19:05:13 +0530

    basic seed data is added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+require 'yaml'
+
+
+companies = YAML.load(File.open(File.expand_path('db/companies.yml')))
+puts 'seeding companies details...'
+companies.each do |key, value|
+  Company.find_or_create_by(name: value['name'],city: value['city'],website: value['website'])
+end
